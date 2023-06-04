abc1c445fd185021d967608ec316d71b50369e73
DungNT2 <nguyentiendunga42002@gmail.com>
2023-06-04 18:16:52 +0700

    push new API

--- src/main/java/com/kma/zing/controller/UploadController.java
+++ src/main/java/com/kma/zing/controller/UploadController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/zing/v1/upload")
 public class UploadController {
